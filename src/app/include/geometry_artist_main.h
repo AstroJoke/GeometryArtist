c7b0f5e621bfdcca818193ff82ab36e2e168ee7d
LeilongWang <wangleilongwork@163.com>
2025-12-11 15:27:19 +0800

    (build fix) 修复了cmake后uic失败的问题 (feat) 添加了mainwindow相关的代码框架及ui文件

---
+++ src/app/include/geometry_artist_main.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+
+#include <QObject>
+#include <QMainWindow>
+#include <QActionGroup>
+
+#include "ui_geometry_artist_main.h"
+
+class QActionGroup;
+class Ui_Geometry_artist_mainwindow;
+
+namespace geomart
+{
+	class BooleanScene;
+	class BooleanViewer;
+	class GeometryArtistMain : public QMainWindow
+	{
+		Q_OBJECT
+
+	public:
+		explicit GeometryArtistMain(QWidget *parent = nullptr, Qt::WindowFlags flags = {});
+		virtual ~GeometryArtistMain() override;
+
+		GeometryArtistMain(const GeometryArtistMain &) = delete;
+		GeometryArtistMain &operator=(const GeometryArtistMain &) = delete;
+		GeometryArtistMain(GeometryArtistMain &&) = delete;
+		GeometryArtistMain &operator=(GeometryArtistMain &&) = delete;
+
+	public Q_SLOTS:
+
+	private:
+		void treatGUI();
+		void treatMenu();
+		void treatAction();
+
+	private:
+		// TODO
+		// std::unique_ptr<GeomScene> scene_manager_;
+		// std::unique_ptr<GeomViewer> viewer_manager_;
+
+	private:
+		Ui_Geometry_artist_mainwindow *ui_ = nullptr;
+		QActionGroup *actgrpShading_ = nullptr;
+	};
+}
---
+++ src/app/src/geometry_artist_main.cpp
@@ -0,0 +1,42 @@
+#include "geometry_artist_main.h"
+
+geomart::GeometryArtistMain::GeometryArtistMain(QWidget *parent, Qt::WindowFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui_ = new Ui_Geometry_artist_mainwindow();
+	ui_->setupUi(this);
+
+	treatGUI();
+	treatMenu();
+	treatAction();
+}
+
+geomart::GeometryArtistMain::~GeometryArtistMain()
+{
+	delete ui_;
+	ui_ = nullptr;
+}
+
+void geomart::GeometryArtistMain::treatGUI()
+{
+	// TODO
+	/*scene_manager_ = std::make_unique<GeomScene>();
+	viewer_manager_ = std::make_unique<GeomViewer>(this);
+	viewer_manager_->setScene(scene_manager_.get());
+	ui_->verticalLayout->addWidget(viewer_manager_.get());*/
+}
+
+void geomart::GeometryArtistMain::treatMenu()
+{
+	// TODO
+}
+
+void geomart::GeometryArtistMain::treatAction()
+{
+	actgrpShading_ = new QActionGroup(this);
+	actgrpShading_->addAction(ui_->actionflat_shading);
+	actgrpShading_->addAction(ui_->actiongouraud_shading);
+	ui_->actionflat_shading->setChecked(true);
+
+	ui_->actiondo_boolean->setChecked(true);
+}
